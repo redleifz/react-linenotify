19113b24e80c336472eb7916448d2c822c696b47
Jongjate <83629650+redleifz@users.noreply.github.com>
2023-01-28 20:59:40 +0700

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import sendlinenotifyRouter from './routes/sendLineNotifyRoutes.js'
 import express from 'express'
 import bodyParser from "body-parser"
+import cors from 'cors'
 
 const app = express()
 const port = 5000
+
+const corsOptions = {
+  origin: 'https://www.jongjate.com',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json())
 app.use(
